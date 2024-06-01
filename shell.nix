8296a7435e4576f5fdc0bf14509e2763dc95f7bc
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-06-01 10:22:46 -0500

    more
      upgrade python from 3.11 to 3.12
      delete requirements.txt
        as I'm no longer using virtualenv

--- shell.nix
+++ shell.nix
@@ -5,12 +5,14 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
 in pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (python311Packages: [
-      # select Python packages here
-      python311Packages.ipython
-      python311Packages.numpy
-      python311Packages.pandas
-      python311Packages.xlrd # to read Excel files
+    (pkgs.python3.withPackages (python312Packages: [
+      python312Packages.ipython
+      python312Packages.mypy
+      python312Packages.numpy
+      python312Packages.pandas
+      python312Packages.pyspark
+      python312Packages.pypika # to build SQL queries
+      python312Packages.xlrd   # to read Excel files
     ]))
   ];
 }
